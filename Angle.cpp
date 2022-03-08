3cba898e8417e850e2444fe52ce9bb13ff64d685
visan <alexandruvisan44@yahoo.com>
2022-03-08 11:10:02 +0200

    Initial commit.

---
+++ Angle.cpp
@@ -0,0 +1,64 @@
+//
+// Created by visan on 3/4/22.
+//
+
+#include "Angle.h"
+
+namespace Trigonometry {
+    unsigned Angle::get_total_seconds() const {
+        return floor(rad * DEG_PER_RAD * ARC_SEC_PER_DEG);
+    }
+
+    double Angle::adjust_angle(double radians) {
+        while (radians < 0) {
+            radians += CIRCLE_SIZE_RAD;
+        }
+        while (radians >= CIRCLE_SIZE_RAD) {
+            radians -= CIRCLE_SIZE_RAD;
+        }
+        return radians;
+    }
+
+    Angle::Angle() : rad(0) {
+
+    }
+
+    Angle::Angle(double _radians) : rad(adjust_angle(_radians)) {
+
+    }
+
+    Angle::Angle(unsigned _degrees, unsigned _minutes, unsigned _seconds) {
+        double degrees = (double) _seconds / ARC_SEC_PER_DEG + (double) _minutes / ARC_SEC_PER_MIN + _degrees;
+        rad = adjust_angle(degrees * RAD_PER_DEG);
+    }
+
+    double Angle::radians() const {
+        return rad;
+    }
+
+    unsigned Angle::degrees() const {
+        return get_total_seconds() / ARC_SEC_PER_DEG;
+    }
+
+    unsigned Angle::minutes() const {
+        return (get_total_seconds() % ARC_SEC_PER_DEG) / ARC_SEC_PER_MIN;
+    }
+
+    unsigned Angle::seconds() const {
+        return (get_total_seconds() % ARC_SEC_PER_DEG) % ARC_SEC_PER_MIN;
+    }
+
+    double Angle::cos() const {
+        //Call cos function from the cmath library
+        return ::cos(rad);
+    }
+
+    double Angle::sin() const {
+        //Call sin function from the cmath library
+        return ::sin(rad);
+    }
+
+    Angle operator+(const Angle &a, const Angle &b) {
+        return Angle(a.rad + b.rad);
+    }
+}
---
+++ Angle.h
@@ -0,0 +1,66 @@
+//
+// Created by visan on 3/4/22.
+//
+
+#ifndef ANGLE_ANGLE_H
+#define ANGLE_ANGLE_H
+
+#include<cmath>
+
+namespace Trigonometry {
+
+    const double RAD_PER_DEG = M_PI / 180;
+
+    const double DEG_PER_RAD = 180 / M_PI;
+
+    const double CIRCLE_SIZE_RAD = 2 * M_PI;
+
+    const unsigned ARC_SEC_PER_MIN = 60;
+
+    const unsigned ARC_SEC_PER_DEG = 3600;
+
+    class Angle {
+    private:
+        double rad;
+
+        //Returns the total of arc-seconds in the angle.
+        unsigned get_total_seconds() const;
+
+        //Clamps the given value of radians between [0,2*PI)
+        static double adjust_angle(double radians);
+
+    public:
+        //Default constructor, create an angle of 0 radians.
+        Angle();
+
+        //Create an angle using radians.
+        explicit Angle(double _radians);
+
+        //Create an angle using sexagesimal measures.
+        Angle(unsigned _degrees, unsigned _minutes, unsigned _seconds);
+
+        //Get the number of radians.
+        double radians() const;
+
+        //Get the number of degrees.
+        unsigned degrees() const;
+
+        //Get the number of minutes.
+        unsigned minutes() const;
+
+        //Get the number of seconds.
+        unsigned seconds() const;
+
+        //Returns the cosine of the angle.
+        double cos() const;
+
+        //Returns the sine of the angle.
+        double sin() const;
+
+        friend Angle operator+(const Angle &a, const Angle &b);
+    };
+
+    Angle operator+(const Angle &a, const Angle &b);
+}
+
+#endif //ANGLE_ANGLE_H
